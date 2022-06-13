8cafd323963b2d6fddad9a9f84232993476cff4b
yavuz <yavuzuluturk@gmail.com>
2022-06-13 23:29:10 +0200

    second commit

---
+++ src/test/java/com/TechProEd/SeleniumPractice/FirstMavenPractice/Day02/TestCase10.java
@@ -0,0 +1,21 @@
+package com.TechProEd.SeleniumPractice.FirstMavenPractice.Day02;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestCase10 {
+
+    WebDriver driver;
+
+    @Before
+    public void setUp(){
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+        driver.manage().window().maximize();
+
+    }
+}
